d80d504c51e54814af70eb6367d29fbb603d1bd1
Miranda Chiu <mirandachiu13@gmail.com>
2020-11-19 11:10:29 -0500

    add yt show logos

--- src/components/DesignTokens/Logo/svgs/index.js
+++ src/components/DesignTokens/Logo/svgs/index.js
@@ -2,14 +2,18 @@ import ATK from './ATK';
 import CCO from './CCO';
 import CIO from './CIO';
 import MysteryRecipe from './MysteryRecipe';
+import PerfectlySeasonal from './PerfectlySeasonal';
 import Proof from './Proof';
 import TheWalkIn from './TheWalkIn';
+import WhatsEatingDan from './WhatsEatingDan';
 
 export default {
   ATK,
   CCO,
   CIO,
   MysteryRecipe,
+  PerfectlySeasonal,
   Proof,
   TheWalkIn,
+  WhatsEatingDan,
 };
