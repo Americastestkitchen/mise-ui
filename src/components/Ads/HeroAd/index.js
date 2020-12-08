bdcbfc88dd15bf73fcb7faa85f62277f2769d9c0
Miranda Chiu <mirandachiu13@gmail.com>
2020-12-08 11:05:49 -0500

    allow book image to overlap with hero card and increase text size on mobile

--- src/components/Ads/HeroAd/index.js
+++ src/components/Ads/HeroAd/index.js
@@ -23,7 +23,6 @@ const HeroAdWrapperTheme = {
     width: ${grid.columnWidth};
 
     ${breakpoint('xs', 'md')`
-      overflow: hidden;
       width: 100%;
     `}
   `,
@@ -91,10 +90,11 @@ const HeroAdContent = styled.div.attrs({
  */
 const HeroAdTitleTheme = {
   default: css`
-    font: ${fontSize.xl}/${lineHeight.sm} ${font.pnb};
+    font: 2.6rem/${lineHeight.sm} ${font.pnb};
     margin-bottom: ${spacing.sm};
 
     ${breakpoint('md')`
+      font-size: ${fontSize.xl};
       text-align: center;
     `}
   `,
