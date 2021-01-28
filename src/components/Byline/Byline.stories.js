d4b4c128343a2cde3180ac3574336187fb63aef1
Miranda Chiu <mirandachiu13@gmail.com>
2021-01-28 10:05:43 -0500

    rename ArticleCard to MarqueeCard and minor Byline and MarqueeCard updates

--- src/components/Byline/Byline.stories.js
+++ src/components/Byline/Byline.stories.js
@@ -1,22 +1,24 @@
 import React from 'react';
 import Byline from './index';
+import { withKnobs, text } from '@storybook/addon-knobs';
 
 export default {
   title: 'Components|Byline',
   component: Byline,
+  decorators: [withKnobs],
 };
 
 export const Default = () => (
   <Byline
-    author="Kevin Pang"
-    authorImageCloudinaryId="Play%20Cast%20Headshots/staff_dan_souza"
-    attribution="Yesterday"
+    author={text('Author Name', 'Kevin Pang')}
+    authorImageCloudinaryId={text('Author Image', 'AKO%20Articles/Author_Headshots/staff_kevin_pang')}
+    attribution={text('Attribution', 'Yesterday')}
   />
 );
 
 export const NoImage = () => (
   <Byline
-    author="Kevin Pang"
-    attribution="Yesterday"
+    author={text('Author Name', 'Kevin Pang')}
+    attribution={text('Attribution', 'Digital Editorial Director')}
   />
 );
---
+++ src/components/Cards/MarqueeCard/MarqueeCard.stories.js
@@ -0,0 +1,31 @@
+import React from 'react';
+import MarqueeCard from './index';
+import styled from 'styled-components';
+import { color, spacing  } from '../../../styles';
+import { withKnobs, select, text } from '@storybook/addon-knobs';
+
+export default {
+  title: 'Components|Cards/MarqueeCard',
+  component: MarqueeCard,
+  decorators: [withKnobs],
+};
+
+const StoryWrapper = styled.div`
+  background-color: ${color.whiteSmoke};
+  padding: ${spacing.sm};
+`;
+export const Default = () => (
+  <StoryWrapper className="story-wrapper">
+    <MarqueeCard
+      author={text('Author Name', 'Kevin Pang')}
+      authorImageCloudinaryId={text('Author Image', 'AKO%20Articles/Author_Headshots/staff_kevin_pang')}
+      backgroundCloudinaryId={text('Background Image', 'AKO Articles/2020 Web Articles/Things_We_Love-01_2')}
+      description={text('Description', "Including a virtual bread project; a punchy, small-batch hot sauce; and a digital encyclopedia of Texas tacos.")}
+      href={text('Link', 'https://www.americastestkitchen.com/articles/2839-how-to-make-your-zoom-thanksgiving-feel-like-the-real-thing')}
+      publishDate={text('Publish Date', 'Yesterday')}
+      siteKey={select('Site Key', ['atk', 'cco', 'cio', 'kids', 'school', 'shop'], 'atk')}
+      stickers={[{ type: 'editorial', text: 'Community' }]}
+      title={text('Title', '10 Things in the Food World We Loved in November')}
+    />
+  </StoryWrapper>
+);
