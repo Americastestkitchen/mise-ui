db5e9db2054c9cfe15c358cefa66e21fa107d651
Miranda Chiu <mirandachiu13@gmail.com>
2021-01-29 12:42:26 -0500

    ARCHIE-1804 lead marquee css updates

--- src/components/Cards/LeadMarqueeCard/LeadMarqueeCard.stories.js
+++ src/components/Cards/LeadMarqueeCard/LeadMarqueeCard.stories.js
@@ -19,7 +19,22 @@ export const Default = () => (
       author={text('Author Name', 'Kevin Pang')}
       authorImageCloudinaryId={text('Author Image', 'AKO%20Articles/Author_Headshots/staff_kevin_pang')}
       backgroundCloudinaryId={text('Background Image', 'AKO%20Articles/2020%20Web%20Articles/ZoomThanksgivinghero')}
-      backgroundColor={select('Background Color', ['#1A3352', '#B25B18', '#321A52', '#1775C2', '#857351', ,'#521a2d','#405700', '#005E71','#167A7A', '#0B3C3D'], "#783681")}
+      backgroundColor={select('Background Color', ['#1A3352', '#B25B18', '#321A52', '#1775C2', '#857351', '#521a2d','#405700', '#005E71','#167A7A', '#0B3C3D'], "#783681")}
+      description="There’s a better way than squinting into a laptop."
+      href={text('Link', 'https://www.americastestkitchen.com/articles/2839-how-to-make-your-zoom-thanksgiving-feel-like-the-real-thing')}
+      siteKey="atk"
+      stickers={[{ type: 'priority', text: 'New' }, { type: 'editorial', text: 'Holiday' }]}
+      title={text('Title', 'How to Make Your Zoom Thanksgiving Feel Like the Real Thing')}
+    />
+  </StoryWrapper>
+);
+
+export const NoAuthorImage = () => (
+  <StoryWrapper className="story-wrapper">
+    <LeadMarqueeCard
+      author={text('Author Name', 'Kevin Pang')}
+      backgroundCloudinaryId={text('Background Image', 'AKO%20Articles/2020%20Web%20Articles/ZoomThanksgivinghero')}
+      backgroundColor={select('Background Color', ['#1A3352', '#B25B18', '#321A52', '#1775C2', '#857351', '#521a2d','#405700', '#005E71','#167A7A', '#0B3C3D'], "#783681")}
       description="There’s a better way than squinting into a laptop."
       href={text('Link', 'https://www.americastestkitchen.com/articles/2839-how-to-make-your-zoom-thanksgiving-feel-like-the-real-thing')}
       siteKey="atk"
