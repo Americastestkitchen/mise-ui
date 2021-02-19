cb05ec8faccdeb564af5c5f60a99962767009e5c
Miranda Chiu <mirandachiu13@gmail.com>
2021-02-19 08:46:43 -0500

    ARCHIE-1844 byline updates

--- src/components/Byline/index.js
+++ src/components/Byline/index.js
@@ -1,6 +1,8 @@
 import React from 'react';
 import PropTypes from 'prop-types';
 import styled from 'styled-components';
+import breakpoint from 'styled-components-breakpoint';
+
 import {
   color,
   font,
@@ -11,39 +13,71 @@ import {
 import PersonHeadShot from '../Cards/shared/PersonHeadShot';
 
 const BylineWrapper = styled.div`
-  align-items: center;
   display: flex;
-  .person-head-shot {
-    margin-right: ${spacing.sm};
-  }
+  flex-direction: column;
 
   span {
     color: ${color.eclipse};
     font: ${fontSize.md}/${lineHeight.sm} ${font.pnr};
-    margin: 0 ${spacing.xsm};
 
     &:first-of-type {
       font: ${fontSize.md}/${lineHeight.sm} ${font.pnb};
-      margin: 0;
-    }
-
-    :last-of-type {
-      margin: 0;
     }
   }
+
+  ${breakpoint('md')`
+    align-items: center;
+    flex-direction: row;
+  `}
+`;
+
+const Author = styled.div.attrs({
+  className: 'byline__author',
+})`
+  align-items: center;
+  display: flex;
+
+  .person-head-shot {
+    margin-right: ${spacing.sm};
+  }
+`;
+
+const Attribution = styled.div.attrs({
+  className: 'byline__attribution',
+})`
+  display: flex;
+  margin-top: ${spacing.sm};
+
+  span:first-child {
+    display: ${props => (props.attribution ? 'none' : 'block')};  
+    margin: 0 ${spacing.xsm};
+  }
+
+  ${breakpoint('md')`
+    margin-top: 0;
+
+    span:first-child {
+      display: block;
+    }
+  `}
 `;
 
 const Byline = ({
   author,
   authorImageCloudinaryId,
   attribution,
+  className,
   imgAlt,
 }) => (
-  <BylineWrapper className={`byline${authorImageCloudinaryId ? '' : ' no-image'}`}>
-    {authorImageCloudinaryId && <PersonHeadShot imgCloudinaryId={authorImageCloudinaryId} size={{ sm: '4' }} imgAlt={imgAlt} />}
-    <span rel="author">{author}</span>
-    {author && attribution && (<span> |</span>)}
-    {attribution && <span>{attribution}</span>}
+  <BylineWrapper className={`byline${authorImageCloudinaryId ? '' : ' no-image'}${className ? ` ${className}` : ''}`}>
+    <Author>
+      {authorImageCloudinaryId && <PersonHeadShot imgCloudinaryId={authorImageCloudinaryId} size={{ sm: '4' }} imgAlt={imgAlt} />}
+      <span rel="author">{author}</span>
+    </Author>
+    <Attribution attribution={attribution}>
+      {author && attribution && (<span> |</span>)}
+      {attribution && <span>{attribution}</span>}
+    </Attribution>
   </BylineWrapper>
 );
 
@@ -54,6 +88,7 @@ Byline.propTypes = {
   authorImageCloudinaryId: PropTypes.string,
   /** Optional field to add custom text information such as publish date or author title */
   attribution: PropTypes.string,
+  className: PropTypes.string,
   /** Optional: Alt text for img */
   imgAlt: PropTypes.string,
 };
@@ -62,6 +97,7 @@ Byline.propTypes = {
 Byline.defaultProps = {
   authorImageCloudinaryId: null,
   attribution: '',
+  className: '',
   imgAlt: '',
 };
 export default Byline;
