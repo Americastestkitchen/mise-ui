bde01d6a3adfe45e7154ac78f7389e03a0d216e7
Jess Bon <jess.bon@americastestkitchen.com>
2021-03-29 06:22:13 -0700

    Updates to Svgs, Resolved tests, Default Icon if svgId not found

---
+++ src/components/Cards/CategoryCard/iconMap.js
@@ -0,0 +1,13 @@
+import ShoppingCart from '../../DesignTokens/Icon/svgs/ShoppingCart';
+import Star from '../../DesignTokens/Icon/svgs/Star';
+import TrendingArrow from '../../DesignTokens/Icon/svgs/TrendingArrow';
+import LandingPlay from '../../DesignTokens/Icon/svgs/LandingPlay';
+
+const IconMap = {
+  play: LandingPlay,
+  shoppingCart: ShoppingCart,
+  star: Star,
+  trendingArrow: TrendingArrow,
+};
+
+export default IconMap;
