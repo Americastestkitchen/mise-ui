926f061d223dafaf996615c05c1b4da4074dbe7b
ttornese <tornese.tom@gmail.com>
2021-03-30 11:19:13 -0400

    creating ToggleRefinement connected instant search component

---
+++ src/components/Algolia/shared/ToggleRefinement/index.js
@@ -0,0 +1,34 @@
+import React from 'react';
+import PropTypes from 'prop-types';
+import { connectRefinementList } from 'react-instantsearch-dom';
+
+import RefinementFilter from '../RefinementFilter/RefinementFilter';
+
+const ToggleRefinement = ({
+  attribute,
+  label,
+  value,
+  ...restProps
+}) => (
+  <div className="toggle-refinement">
+    <RefinementFilter
+      attribute={attribute}
+      filterType="toggleRefinement"
+      includeCount={false}
+      label={label}
+      value={value}
+      {...restProps}
+    />
+  </div>
+);
+
+ToggleRefinement.propTypes = {
+  /** Algolia attribute that is used to pull refinement values. */
+  attribute: PropTypes.string.isRequired,
+  /** Filter label */
+  label: PropTypes.string.isRequired,
+  /** Value of filter to be used for refining results. */
+  value: PropTypes.oneOfType([PropTypes.string, PropTypes.array], PropTypes.string).isRequired,
+};
+
+export default connectRefinementList(ToggleRefinement);
