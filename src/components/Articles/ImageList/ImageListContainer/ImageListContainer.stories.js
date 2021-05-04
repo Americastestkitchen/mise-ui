3fb2f89540c8624c0862b08c1ba9f7d9c07c0955
Jess Bon <jess.bon@americastestkitchen.com>
2021-05-04 13:54:41 -0700

    Updating Imports w/ New Paths

---
+++ src/components/Articles/ImageList/ImageListContainer/ImageListContainer.stories.js
@@ -0,0 +1,46 @@
+import React from 'react';
+
+import ImageListContainer from './index';
+import ImageListItem from '../ImageListItem/index';
+
+export default {
+    title: 'Components|Articles/ImageListContainer',
+    component: ImageListContainer,
+};
+
+const images = [{
+  altText: "picture of a thing",
+  cloudinaryId: "TnT/2020/1_CCJJ_Dill%20Pickles/SPS_Dill_Pickle_Hero_123",
+  content: "\u003cp\u003eThere are many things about this picture to say. There are many things about this picture to say. There are many things about this picture to say.\u003cp\u003e",
+  lazy: true
+}, {
+  altText: "picture of a thing",
+  cloudinaryId: "TnT/2020/1_CCJJ_Dill%20Pickles/SPS_Dill_Pickle_Hero_123",
+  content: "\u003cp\u003eThere are many things about this picture to say. There are many things about this picture to say. There are many things about this picture to say.\u003cp\u003e",
+  lazy: true
+}];
+
+const image = [{
+  altText: "picture of a thing",
+  cloudinaryId: "TnT/2020/1_CCJJ_Dill%20Pickles/SPS_Dill_Pickle_Hero_123",
+  content: "\u003cp\u003eThere are many things about this picture to say. There are many things about this picture to say. There are many things about this picture to say.\u003cp\u003e",
+  lazy: true
+}];
+
+export const defaultBoxOneImage = () => (
+  <ImageListContainer
+    children={image.map((el => <ImageListItem {...el}/>))}
+    className="image-list__container"
+    title="These are my images"
+    width="default"
+  />
+);
+
+export const wideBoxMultipleImages = () => (
+  <ImageListContainer
+    children={images.map((el, idx) => <ImageListItem key={`${el.altText}-${idx}`} {...el} width="wide"/>)}
+    className="image-list__container"
+    title="These are my images"
+    width="wide"
+  />
+);
