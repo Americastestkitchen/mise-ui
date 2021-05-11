d5323c4b2d03ac439b4ec4addccbf8052488cd81
Jess Bon <jess.bon@americastestkitchen.com>
2021-05-11 14:07:13 -0700

    Styling & Prop Adjustments for Article Components

--- src/components/Articles/ArticlePhotoCollection/imagesResponse.js
+++ src/components/Articles/ArticlePhotoCollection/imagesResponse.js
@@ -3,7 +3,7 @@ const images = [{
   attributes: {
     photo: {
       publicId: 'TnT/2020/1_CCJJ_Dill%20Pickles/STP_Spear_Crunch_102',
-      width: 400,
+      contentWidth: 400,
       height: 400,
       format: 'gif',
     },
@@ -14,7 +14,7 @@ const images = [{
   attributes: {
     photo: {
       publicId: 'TnT/2020/1_CCJJ_Dill%20Pickles/STP_Spear_Crunch_102',
-      width: 400,
+      contentWidth: 400,
       height: 400,
       format: 'gif',
     },
@@ -25,7 +25,7 @@ const images = [{
   attributes: {
     photo: {
       publicId: 'TnT/2020/1_CCJJ_Dill%20Pickles/STP_Spear_Crunch_102',
-      width: 400,
+      contentWidth: 400,
       height: 400,
       format: 'gif',
     },
