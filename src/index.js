f5a4f52c1f141b39d68a16cec6c9e4dea18f7410
ttornese <tornese.tom@gmail.com>
2021-05-13 13:49:55 -0400

    ARCHIE-2498: export ArticleTextBlock for use in espresso

--- src/index.js
+++ src/index.js
@@ -6,6 +6,7 @@ import AccordionRefinementList from './components/Algolia/shared/AccordionRefine
 import ArticleFigcaption from './components/Articles/shared/ArticleFigcaption';
 import ArticleImage from './components/Articles/ArticleImage';
 import ArticlePhotoCollection from './components/Articles/ArticlePhotoCollection';
+import ArticleTextBlock from './components/Articles/ArticleTextBlock';
 import Badge from './components/Badge';
 import BookCarouselAd from './components/Ads/ReviewsAds/BookCarouselAd';
 import breakpoints from './styles/breakpoints';
@@ -86,6 +87,7 @@ export {
   ArticleFigcaption,
   ArticleImage,
   ArticlePhotoCollection,
+  ArticleTextBlock,
   AudioPlayer,
   Badge,
   BookCarouselAd,
