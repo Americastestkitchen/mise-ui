23d7bd81163560fe6bf68393f270a02bdb92fdc8
Tom Tornese <tornese.tom@gmail.com>
2021-08-03 16:08:46 -0400

    ARCHIE-2175: themeing PullQuote, adding control addon to storybook

--- .storybook/main.js
+++ .storybook/main.js
@@ -9,5 +9,6 @@ module.exports = {
     '@storybook/addon-viewport/register',
     '@whitespace/storybook-addon-html',
     '@storybook/addon-notes/register',
+    '@storybook/addon-controls',
   ],
 };
