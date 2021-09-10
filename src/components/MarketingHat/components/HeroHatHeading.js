e1b6ad984146120beec89827fdc14bd7bea9440e
Jason Aslakson <jason.aslakson@americastestkitchen.com>
2021-09-10 14:04:36 -0400

    atk marketing hat images

---
+++ src/components/MarketingHat/components/HeroHatHeading.js
@@ -0,0 +1,27 @@
+import styled from 'styled-components';
+import breakpoint from 'styled-components-breakpoint';
+import {
+  font,
+  fontSize,
+  spacing,
+} from '../../../styles';
+
+const HeroHatHeading = styled.h2.attrs({
+  className: 'hero-hat__heading',
+})`
+  font: 2.6rem/${spacing.lg} ${font.mwr};
+  font-weight: 600;
+  margin-bottom: 0.6rem;
+
+  ${breakpoint('md')`
+    font-size: ${fontSize.xxl};
+    line-height: 3.9rem;
+  `}
+
+  ${breakpoint('xlg')`
+    line-height: 4.6rem;
+    white-space: nowrap;
+  `}
+`;
+
+export default HeroHatHeading;
---
+++ src/components/MarketingHat/components/RegistrantSubmit.js
@@ -0,0 +1,30 @@
+import styled from 'styled-components';
+import breakpoint from 'styled-components-breakpoint';
+import { color } from '../../../styles';
+
+const RegistrantSubmit = styled.button.attrs({
+  className: 'hero-hat__button',
+})`
+  background-color: ${color.frog};
+  color: ${color.white};
+  text-transform: uppercase;
+  max-width: 25rem;
+
+  &:hover {
+    background-color: ${color.darkFrog};
+  }
+
+  ${breakpoint('md')`
+    max-width: 28rem;
+    min-width: 28rem;
+  `}
+
+  ${breakpoint('xlg')`
+    margin-bottom: 2%;
+    margin-right: 19%;
+    max-width: 22.6rem;
+    min-width: 22.6rem;
+  `}
+`;
+
+export default RegistrantSubmit;
