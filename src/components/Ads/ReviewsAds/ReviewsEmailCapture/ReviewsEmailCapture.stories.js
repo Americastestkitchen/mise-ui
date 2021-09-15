e0b6917acc3e36e5a7d03fd943382fe81ac5bac9
Tom Tornese <tornese.tom@gmail.com>
2021-09-15 09:04:35 -0400

    ARCHIE-2809: more CIO and CCO review qa feedback

--- src/components/Ads/ReviewsAds/ReviewsEmailCapture/ReviewsEmailCapture.stories.js
+++ src/components/Ads/ReviewsAds/ReviewsEmailCapture/ReviewsEmailCapture.stories.js
@@ -1,39 +1,23 @@
 import React from 'react';
-import { ThemeProvider } from 'styled-components';
-import { withKnobs } from '@storybook/addon-knobs';
 
-import { breakpoints } from '../../../../styles';
 import ReviewsEmailCapture from './index';
+import { siteKey } from '../../../../config/argTypes';
+import { addThemedWrapper } from '../../../../config/decorators';
 
 export default {
   title: 'Components/Ads/ReviewsAds/ReviewsEmailCapture',
   component: ReviewsEmailCapture,
-  decorators: [withKnobs],
+  decorators: [ addThemedWrapper() ],
+  argTypes: { siteKey },
 };
 
-const onSubmit = () => {
-  // mixpanel & success prop from espresso
+const Template = (args) => <ReviewsEmailCapture {...args} />;
+
+export const Default = Template.bind({});
+Default.args = {
+  description: 'Shop smarter with our ATK Reviews team\'s expert guides and top picks',
+  inputId: 'review-email-capture__detail',
+  onSubmit: () => {},
+  siteKey: 'atk',
+  title: 'Sign up for the Well-Equipped Cook Newsletter',
 };
-
-export const ReviewsEmailCaptureNoSuccess = () => (
-  <ThemeProvider theme={{ breakpoints }}>
-    <ReviewsEmailCapture
-      description="Shop smarter with our ATK Reviews team's expert guides and top picks"
-      inputId="review-email-capture__detail"
-      onSubmit={onSubmit}
-      title="Sign up for the Well-Equipped Cook Newsletter"
-    />
-  </ThemeProvider>
-)
-
-export const ReviewsEmailCaptureSuccess = () => (
-  <ThemeProvider theme={{ breakpoints }}>
-    <ReviewsEmailCapture
-      description="Shop smarter with our ATK Reviews team's expert guides and top picks"
-      inputId="review-email-capture__detail"
-      onSubmit={onSubmit}
-      success={true}
-      title="Sign up for the Well-Equipped Cook Newsletter"
-    />
-  </ThemeProvider>
-)
