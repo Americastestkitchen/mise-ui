b3148fb7be6b6b995099f4d289a92d508b54208f
Jenner Thomas <jthomas@jthomas-mbp.attlocal.net>
2021-11-22 11:30:39 -0500

    reviews marketing hat

--- src/components/Ads/ReviewsAds/ReviewsMarketingHat/index.js
+++ src/components/Ads/ReviewsAds/ReviewsMarketingHat/index.js
@@ -8,45 +8,54 @@ import EmailForm from '../../../Forms/EmailForm';
 import { getImageUrl } from '../../../../lib/cloudinary';
 import { color, font, fontSize, letterSpacing, spacing } from '../../../../styles';
 
-const AdImage = styled.picture`
-  max-height: 26.7rem;
+const AdImage = styled.div`
+  height: 20rem;
+  max-height: 31.5rem;
   width: 100%;
   z-index: 1;
+  background-image: url(${({ images }) => (images.mobileAsset)});
+  background-repeat: no-repeat;
+  background-size: cover;
 
-  img {
-    max-height: 31.5rem;
-    min-width: 100%;
-  }
 
   ${breakpoint('md')`
+    background-image: url(${({ images }) => (images.tabletAsset ? images.tabletAsset : images.desktopAsset)});
+    height: 30.7rem;
     max-height: 30.7rem;
     width: 50%;
     z-index: 2;
+    min-height: 100%;
+    max-width: 77.7rem;
+  `}
 
-    img {
-      max-height: 30.7rem;
-      max-width: 77.7rem;
-    }
+  ${breakpoint('lg')`
+    background-image: url(${({ images }) => (images.desktopAsset)});
   `}
 
   ${breakpoint('xlg')`
+    background-image: url(${({ images }) => (images.desktopLgAsset ? images.desktopLgAsset : images.desktopAsset)});
     min-width: 50%;
-
-    img {
-      min-height: 100%;
-    }
   `}
 `;
 
 const ContentSection = styled.div`
   background-color: ${color.bigStone};
+  color: ${color.white};
+  top: 10.5rem;
+
+  &.dollarOffer {
+    background-color: ${color.dawnPink};
+    color: ${color.eclipse};
+    top: 7.5rem;
+  }
+
   left: 1.7rem;
   padding: ${spacing.sm} 1.9rem ${spacing.md} 2rem;
   position: absolute;
   text-align: left;
-  top: 15.5rem;
   width: calc(100% - 3.4rem);
   z-index: 2;
+  margin-bottom: 3rem;
 
   &.anon-user {
     padding: ${spacing.sm} 1.9rem ${spacing.xxsm} 2rem;
@@ -57,6 +66,7 @@ const ContentSection = styled.div`
     max-width: 50%;
     padding: ${spacing.xlg} 3.6rem ${spacing.xlg} 1.8rem;
     position: static;
+    margin: 0;
 
     &.anon-user {
       padding: 1.4rem 1.8rem;
@@ -93,8 +103,26 @@ const ContentWrapper = styled.div`
   }
 
   .email-form__how {
-    color: ${color.white};
     font: 1rem/1.2rem ${font.pnr};
+    color: ${color.white};
+  }
+
+  &.dollarOffer {
+    h2 {
+      margin-bottom: 0;
+      ${breakpoint('lg')`
+        font-size: 2rem;
+      `}
+    }
+    h3 {
+      font: bold 2.8rem ${font.mwr};
+      ${breakpoint('lg')`
+        font-size: ${fontSize.xxl};
+      `}
+    }
+    .email-form__how {
+      color: ${color.eclipse};
+    }
   }
 
   ${breakpoint('xs')`
@@ -102,9 +130,9 @@ const ContentWrapper = styled.div`
       padding: 0;
     }
   `}
-  
+
   ${breakpoint('md')`
-    width: 34rem;
+    width: 35rem;
 
     .email-form {
       ${({ isAnonymous }) => (
@@ -126,8 +154,12 @@ const ContentWrapper = styled.div`
     }
   `}
 
+  ${breakpoint('lg')`
+    width: 42rem;
+  `}
+
   ${breakpoint('xlg')`
-    width: 54.5rem;
+    width: 55.5rem;
 
     .email-form {
       flex-direction: row;
@@ -154,8 +186,7 @@ const ContentWrapper = styled.div`
 `;
 
 const Description = styled.p`
-  color: ${color.white};
-  font: ${fontSize.md}/2.1rem ${font.pnr};
+  font: ${fontSize.md}/1.9rem ${font.pnr};
   letter-spacing: normal;
   margin-bottom: ${spacing.sm};
 
@@ -177,28 +208,25 @@ const Description = styled.p`
 `;
 
 const Headline = styled.h2`
-  color: ${color.white};
-  font: ${fontSize.sm}/1.7rem ${font.pnb};
+  font: ${fontSize.md} ${font.pnb};
   letter-spacing: 1.4px;
-  margin-bottom: ${spacing.xsm};
   text-transform: uppercase;
+  margin-bottom: 1.2rem;
 
   ${breakpoint('md')`
-    font: ${fontSize.lg}/2rem ${font.pnb};
+    font: ${fontSize.lg}/3rem ${font.pnb};
     letter-spacing: 1.8px;
-    margin-bottom: 1rem;
   `}
 
-  ${breakpoint('xlg')`
-    font: ${fontSize.lg}/3rem ${font.pnb};
-    margin-bottom: 1.2rem;
+  ${breakpoint('lg')`
+    font-size: ${fontSize.lg};
   `}
 `;
 
 const MarketingHatWrapper = styled.div`
   display: flex;
   flex-direction: column;
-  margin-bottom: 20rem;
+  margin-bottom: 20.5rem;
   width: 100%;
 
   @media print {
@@ -206,11 +234,11 @@ const MarketingHatWrapper = styled.div`
   }
 
   &.anon-user {
-    margin-bottom: 26rem;
+    margin-bottom: 23rem;
   }
 
   ${breakpoint('sm')`
-    margin-bottom: 15rem;
+    margin-bottom: 16rem;
 
     &.anon-user {
       margin-bottom: 20rem;
@@ -227,14 +255,14 @@ const MarketingHatWrapper = styled.div`
   `}
 `;
 
-const RegistrantSubmit = styled.a`
+const RegistrantSubmit = styled.button`
   align-items: center;
   background-color: ${color.frog};
   color: ${color.white};
   display: flex;
-  font: ${fontSize.lg}/2rem ${font.pnb};
+  font: ${fontSize.md}/2rem ${font.pnb};
   justify-content: center;
-  letter-spacing: ${letterSpacing.cta};
+  letter-spacing: ${letterSpacing.md};
   max-height: ${spacing.xlg};
   min-height: ${spacing.xlg};
   max-width: 100%;
@@ -250,56 +278,37 @@ const RegistrantSubmit = styled.a`
 `;
 
 const Title = styled.h3`
-  color: ${color.white};
-  font: 3rem/3rem ${font.pnb};
-  letter-spacing: normal;
-  margin-bottom: ${spacing.xxsm};
+  font: bold 2.8rem/3rem ${font.pnb};
 
-  ${breakpoint('md')`
-    font: 4rem/3rem ${font.pnb};
-    margin-bottom: ${spacing.sm};
+  letter-spacing: normal;
+  margin-bottom: ${spacing.xsm};
+
+  ${breakpoint('lg')`
+    font-size: 4rem;
   `}
 
   ${breakpoint('xlg')`
-    margin-bottom: ${spacing.xsm};
     max-height: 4.9rem;
     min-height: 4.9rem;
+    margin-bottom: ${spacing.xxsm};
   `}
 `;
 
 const ReviewsMarketingHat = ({
   buttonText,
   description,
-  desktopAsset,
   headline,
-  incode,
   inputId,
+  images,
   isAnonymous,
-  mdc,
-  mobileAsset,
+  promoId,
   onSubmit,
   title,
 }) => (
   <MarketingHatWrapper className={isAnonymous ? 'anon-user' : ''}>
-    <AdImage>
-      <source
-        srcSet={getImageUrl(desktopAsset, 'reviewsMarketingHat', 307, 'fit')}
-        media="(min-width: 1136px)"
-      />
-      <source
-        srcSet={getImageUrl(mobileAsset, 'reviewsMarketingHat', 307, 'fit', 512)}
-        media="(min-width: 768px)"
-      />
-      <img
-        alt=""
-        className="marketing-hat__image"
-        crossOrigin="anonymous"
-        decoding="async"
-        src={getImageUrl(mobileAsset, 'reviewsMarketingHat', 267, 'fit', 450)}
-      />
-    </AdImage>
-    <ContentSection className={isAnonymous ? 'anon-user' : ''}>
-      <ContentWrapper className={isAnonymous ? 'anon-user' : ''} isAnonymous={isAnonymous}>
+    <AdImage images={images} />
+    <ContentSection className={`${promoId} ${isAnonymous ? 'anon-user' : ''}`}>
+      <ContentWrapper className={`${promoId} ${isAnonymous ? 'anon-user' : ''}`} isAnonymous={isAnonymous}>
         <Headline>{headline}</Headline>
         <Title>{title}</Title>
         <Description className={isAnonymous ? 'anon-user' : ''}>{description}</Description>
@@ -313,7 +322,6 @@ const ReviewsMarketingHat = ({
           />
         ) : (
           <RegistrantSubmit
-            href={`/order?mdc=${mdc}&incode=${incode}`}
             onClick={onSubmit}
           >
             {buttonText}
@@ -327,15 +335,18 @@ const ReviewsMarketingHat = ({
 ReviewsMarketingHat.propTypes = {
   buttonText: PropTypes.string.isRequired,
   description: PropTypes.string.isRequired,
-  desktopAsset: PropTypes.string,
   headline: PropTypes.string.isRequired,
-  incode: PropTypes.string.isRequired,
+  images: PropTypes.shape({
+    desktopAsset: PropTypes.string,
+    desktopLgAsset: PropTypes.string,
+    tabletAsset: PropTypes.string,
+    mobileAsset: PropTypes.string,
+  }),
   /** Input Id used in email form to shift focus on error */
   inputId: PropTypes.string.isRequired,
   /** Remove Email Capture in instances where user is not anon */
   isAnonymous: PropTypes.bool.isRequired,
-  mdc: PropTypes.string.isRequired,
-  mobileAsset: PropTypes.string,
+  promoId: PropTypes.string.isRequired,
   /** Function that redirects & fires mixpanel w/ corresponding incodes */
   onSubmit: PropTypes.func.isRequired,
   title: PropTypes.string.isRequired,
@@ -343,8 +354,10 @@ ReviewsMarketingHat.propTypes = {
 };
 
 ReviewsMarketingHat.defaultProps = {
-  desktopAsset: 'ATK Reviews Ads/PansHat_Desktop_3x.jpg',
-  mobileAsset: 'ATK Reviews Ads/PansHat_Mobile_3x.jpg',
+  images: {
+    desktopAsset: 'https://res.cloudinary.com/hksqkdlah/image/upload/v1621261895/ATK%20Reviews%20Ads/PansHat_Desktop_3x.jpg',
+    mobileAsset: 'https://res.cloudinary.com/hksqkdlah/image/upload/v1621261948/ATK%20Reviews%20Ads/PansHat_Mobile_3x.jpg',
+  },
 };
 
 export default ReviewsMarketingHat;
