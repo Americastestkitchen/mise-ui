3fec2e212dc8e396cc99ba428b454a768cd588b2
Harry Scheuerle <harry.scheuerle@americastestkitchen.com>
2021-11-28 11:08:51 -0500

    ARCHIE-3035: Related Content Card

---
+++ src/components/Cards/RelatedContentCard/RelatedContentCard.stories.tsx
@@ -0,0 +1,41 @@
+/* eslint-disable react/require-default-props */
+import React from 'react';
+import { ThemeProvider } from 'styled-components';
+import RelatedContentCard, { RelatedContentCardProps } from './RelatedContentCard';
+import { defaultTheme, setBackground, storybookParameters, wrapKnobs } from '../../../config/shared.stories';
+
+export default {
+  title: 'Components/Cards/RelatedContentCard',
+  component: RelatedContentCard,
+  ...storybookParameters,
+};
+
+type PreviewProps = { theme?: any, props?: Partial<RelatedContentCardProps> };
+
+const defaultArgs = {
+  href: '/',
+  src: 'https://res.cloudinary.com/hksqkdlah/image/upload/c_fill,f_auto,g_faces:auto,q_auto:low,w_200,ar_1:1/v1592840035/mise-play/feature-card-wide.jpg',
+  headline: 'Recipe',
+  title: 'Whole Roast Snapper with Citrus Vinaigrette',
+  body: 'To serve up an impressive dish of roasted red snapper, we started by making shallow slashes in the skin to ensure even cooking and seasoning; this step also allowed us to gauge the doneness of the fish easily.',
+};
+
+const PreviewRelatedContentCard = ({ theme, props }: PreviewProps) => (
+  <ThemeProvider theme={{ ...defaultTheme, ...theme }}>
+    <RelatedContentCard {...wrapKnobs({ ...defaultArgs, ...props })} />
+  </ThemeProvider>
+);
+
+export const AtkNoLink = () => <PreviewRelatedContentCard theme={{ siteKey: 'atk' }} />;
+export const AtkWithLink = () => <PreviewRelatedContentCard theme={{ siteKey: 'atk' }} props={{ link: 'Save 26% Right Now' }} />;
+export const AtkWithButton = () => <PreviewRelatedContentCard theme={{ siteKey: 'atk' }} props={{ link: 'Save 26% Right Now', withButton: true }} />;
+
+export const CcoNoLink = () => <PreviewRelatedContentCard theme={{ siteKey: 'cco' }} />;
+export const CcoWithLink = () => <PreviewRelatedContentCard theme={{ siteKey: 'cco' }} props={{ link: 'Save 26% Right Now' }} />;
+export const CcoWithButton = () => <PreviewRelatedContentCard theme={{ siteKey: 'cco' }} props={{ link: 'Save 26% Right Now', withButton: true }} />;
+setBackground([CcoNoLink, CcoWithLink, CcoWithButton], 'cco');
+
+export const CioNoLink = () => <PreviewRelatedContentCard theme={{ siteKey: 'cio' }} />;
+export const CioWithLink = () => <PreviewRelatedContentCard theme={{ siteKey: 'cio' }} props={{ link: 'Save 26% Right Now' }} />;
+export const CioWithButton = () => <PreviewRelatedContentCard theme={{ siteKey: 'cio' }} props={{ link: 'Save 26% Right Now', withButton: true }} />;
+setBackground([CioNoLink, CioWithLink, CioWithButton], 'cio');
---
+++ src/components/Cards/RelatedContentCard/RelatedContentCard.tsx
@@ -0,0 +1,218 @@
+import React, { ComponentPropsWithoutRef, PropsWithChildren, useContext } from 'react';
+import styled, { css, ThemeContext } from 'styled-components';
+import { withThemes } from '../../../styles';
+import AffiliateLink from '../shared/AffiliateLink';
+import Image from '../shared/Image';
+
+const themeHighlight = css`
+  ${withThemes({
+    default: css`background-color: rgb(209, 252, 255);`,
+    atk: css`background-color: rgb(209, 252, 255);`,
+    cco: css`background-color: rgb(230, 241, 255);`,
+    cio: css`background-color: rgb(255, 245, 194);`,
+  })}
+`;
+
+const themedUnderline = css`
+  ${withThemes({
+    default: css`background-image: linear-gradient(transparent 91%, rgb(143, 222, 227) 91%);`,
+    atk: css`background-image: linear-gradient(transparent 91%, rgb(143, 222, 227) 91%);`,
+    cco: css`background-image: linear-gradient(transparent 91%, rgb(104, 158, 223) 91%);`,
+    cio: css`background-image: linear-gradient(transparent 91%, rgb(238, 214, 129) 91%);`,
+  })}
+`;
+
+const cssHeadlineFont = css`
+  font-family: 'proximaNovaBold', sans-serif;
+  font-size: 12px;
+  line-height: 1.17;
+  letter-spacing: 1.92px;
+  text-transform: uppercase;
+  ${withThemes({
+    default: css`color: #4d7a7c;`,
+    atk: css`color: #4d7a7c;`,
+    cco: css`color: #416490;`,
+    cio: css`color: #94856b;`,
+  })}
+`;
+
+const cssTitleFont = css`
+  font-family: 'proximaNovaBold', sans-serif;
+  font-size: 18px;
+  line-height: 1.17;
+  color: #3d3d3d;
+`;
+
+const cssBodyFont = css`
+  font-family: 'proximaNovaRegular', sans-serif;
+  font-size: 16px;
+  line-height: 1.25;
+  ${withThemes({
+    default: css`color: #3d3d3d;`,
+    cio: css`color: #3f2b1e;`,
+  })}
+`;
+
+const cssLinkTextFont = css`
+  font-family: 'proximaNovaBold', sans-serif;
+  font-size: 16px;
+  line-height: 1.13;
+  color: #3d3d3d;
+  transition: background-color 0.2s ease-in-out 0s;
+  ${themedUnderline}
+  &:hover {
+    ${themeHighlight}
+  }
+`;
+
+const cssLeadingCenter = css`
+  display: flex;
+  flex-direction: column;
+  justify-content: center;
+  align-items: flex-start;
+`;
+
+const cssCenter = css`
+  display: flex;
+  justify-content: center;
+  align-items: center;
+`;
+
+const cssLineClamp = (lines = 1) => css`
+  display: -webkit-box;
+  -webkit-line-clamp: ${lines};
+  -webkit-box-orient: vertical;
+  overflow: hidden;
+`;
+
+const Headline = styled.span`
+  ${cssHeadlineFont}
+  margin-bottom: 8px;
+`;
+
+const Title = styled.span`
+  ${cssTitleFont}
+  margin-bottom: 8px;
+`;
+
+const Body = styled.span`
+  ${cssBodyFont}
+  ${cssLineClamp(3)}
+  margin-bottom: 8px;
+`;
+
+const LinkText = styled.a`
+  ${cssLinkTextFont}
+  padding: 4px 0;
+`;
+
+const LinkWrapper = styled.div`
+  padding-top: 8px;
+`;
+
+const ImageWrapper = styled.div`
+  ${cssCenter}
+  flex-shrink: 0;
+  height: 200px;
+  width: 200px;
+`;
+
+const Content = styled.div`
+  ${cssLeadingCenter}
+  padding: 16px;
+`;
+
+const Card = styled.a`
+  ${cssCenter}
+  background-color: #fff;
+  ${withThemes({
+    cco: css`
+      box-sizing: border-box;
+      border: solid 3px #f4f4f4;
+    `,
+  })}
+`;
+
+type WideCardWrapperProps = PropsWithChildren<{src: string}> & ComponentPropsWithoutRef<'a'>;
+
+export function Wrapper({ src, children, ...anchorProps }: WideCardWrapperProps) {
+  const theme = useContext(ThemeContext);
+  const imageSize = theme.siteKey === 'cco' ? 180 : 200;
+
+  return (
+    <Card {...anchorProps}>
+      <ImageWrapper>
+        <Image
+          width={imageSize}
+          height={imageSize}
+          aria-hidden="true"
+          imageUrl={src}
+          imageAlt=""
+          lazy={false}
+        />
+      </ImageWrapper>
+      <Content>{children}</Content>
+    </Card>
+  );
+}
+
+/** Composable component API or references for styled components classNames. */
+export const WideCard = {
+  Wrapper,
+  Headline,
+  Title,
+  Body,
+  LinkWrapper,
+  LinkText,
+  AffiliateLink,
+};
+
+export type RelatedContentCardProps = {
+  /** Href attribute value for card link. */
+  href: string;
+  /**
+   * Href attribute for AffiliateLink or text link.
+   * Requires props.link to be provided.
+   * If props.link is provided without this attribute, href will be used.
+   */
+  buttonHref?: string;
+  /** Src attribute value for image. */
+  src: string;
+  /** Text content, 1st at top of card. */
+  headline: string;
+  /** Text content, 2nd from top of card. */
+  title: string;
+  /** Text content, 3rd from top of card. */
+  body: string;
+  /** If provided, render button or text link depending on withButton prop. */
+  link?: string;
+  /** Show button link instead of text link. */
+  withButton?: boolean;
+}
+
+/** Preview helper, example implemenation, and validating exports */
+export default function RelatedContentCard({
+  href,
+  buttonHref,
+  src,
+  headline,
+  title,
+  body,
+  link,
+  withButton,
+}: RelatedContentCardProps) {
+  return (
+    <WideCard.Wrapper href={href} src={src}>
+      <WideCard.Headline>{headline}</WideCard.Headline>
+      <WideCard.Title as="h4">{title}</WideCard.Title>
+      <WideCard.Body>{body}</WideCard.Body>
+      <WideCard.LinkWrapper>
+        {!!link && !!withButton ? (
+          <WideCard.AffiliateLink text={link} url={buttonHref || href} />
+        ) : (
+          <WideCard.LinkText>{link}</WideCard.LinkText>
+        )}
+      </WideCard.LinkWrapper>
+    </WideCard.Wrapper>
+  );
+}
