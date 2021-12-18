cf97b21d0fa96dc5a9a1f1008e8ed4a558684ffa
Harry Scheuerle <harry.scheuerle@americastestkitchen.com>
2021-12-18 09:42:06 -0500

    Styled Components has moved! Make sure you're downloading it from here: https://marketplace.visualstudio.com/items?itemName=styled-components.vscode-styled-components. The jpoissonnier.vscode-styled-components version will recieve no more updates.

--- scripts/setup.js
+++ scripts/setup.js
@@ -7,7 +7,7 @@ const { warn, execLog } = require('./_util');
     'vscode enable and disable commands are not working',
     'go to extensions and make sure workspace recommended plugins are installed',
   );
-  await execLog('code --install-extension jpoissonnier.vscode-styled-components');
+  await execLog('code --install-extension styled-components.vscode-styled-components');
   await execLog('code --install-extension editorconfig.editorconfig');
   await execLog('code --install-extension dbaeumer.vscode-eslint');
   await execLog('code --install-extension wix.vscode-import-cost');
