84293b8933102b2eb271d020fe616e68f80419a4
Harys Vizcaino <harysvizcaino@gmail.com>
2022-01-03 13:41:25 -0400

    ARCHIE-3164 change default color on news lettere
    ARCHIE-3164 add success example
    
    ARCHIE-3164 add success example

--- src/components/Ads/ReviewsAds/ReviewsEmailCapture/ReviewsEmailCapture.stories.js
+++ src/components/Ads/ReviewsAds/ReviewsEmailCapture/ReviewsEmailCapture.stories.js
@@ -9,33 +9,48 @@ export default {
   component: ReviewsEmailCapture,
   decorators: [ addThemedWrapper() ],
   argTypes: { siteKey },
+  onSubmit: () => {},
+  success: false
 };
 
-const Template = (args) => <ReviewsEmailCapture {...args} />;
+const Template = (args) => <ReviewsEmailCapture {...args} />
 
 export const Default = Template.bind({});
 Default.args = {
   description: 'Shop smarter with our ATK Reviews team\'s expert guides and top picks',
   inputId: 'review-email-capture__detail',
-  onSubmit: () => {},
   siteKey: 'atk',
   title: 'Sign up for the Well-Equipped Cook Newsletter',
+  onSubmit: () => {},
+  success: false
 };
 
 export const Cio = Template.bind({});
 Cio.args = {
   description: 'Shop smarter with our ATK Reviews team\'s expert guides and top picks',
   inputId: 'review-email-capture__detail',
-  onSubmit: () => {},
   siteKey: 'cio',
   title: 'Sign up for the Well-Equipped Cook Newsletter',
+  onSubmit: () => {},
+  success: false
 };
 
 export const Cco = Template.bind({});
 Cco.args = {
   description: 'Shop smarter with our ATK Reviews team\'s expert guides and top picks',
   inputId: 'review-email-capture__detail',
-  onSubmit: () => {},
   siteKey: 'cco',
   title: 'Sign up for the Well-Equipped Cook Newsletter',
+  onSubmit: () => {},
+  success: false
+};
+
+export const Success = Template.bind({});
+Success.args = {
+  description: 'Shop smarter with our ATK Reviews team\'s expert guides and top picks',
+  inputId: 'review-email-capture__detail',
+  siteKey: 'cco',
+  title: 'Sign up for the Well-Equipped Cook Newsletter',
+  onSubmit: () => {},
+  success: true
 };
