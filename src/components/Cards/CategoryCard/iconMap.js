db2d3d0f1bab7e0441bbb33baae566a497b20b24
Jess Bon <jess.bon@americastestkitchen.com>
2022-01-27 11:49:43 -0800

    Fixing linter for travis

--- src/components/Cards/CategoryCard/iconMap.js
+++ src/components/Cards/CategoryCard/iconMap.js
@@ -1,13 +1,22 @@
+import CCORooster from '../../DesignTokens/Icon/svgs/CCORooster';
+import CookbookCollection from '../../DesignTokens/Icon/svgs/CookbookCollection';
+import LandingPlay from '../../DesignTokens/Icon/svgs/LandingPlay';
+import LatestReviews from '../../DesignTokens/Icon/svgs/LatestReviews';
+import RecipeCard from '../../DesignTokens/Icon/svgs/RecipeCardUpdate';
+import ReviewsRibbon from '../../DesignTokens/Icon/svgs/ReviewsRibbon';
 import ShoppingCart from '../../DesignTokens/Icon/svgs/ShoppingCart';
 import TrendingArrow from '../../DesignTokens/Icon/svgs/TrendingArrow';
-import LandingPlay from '../../DesignTokens/Icon/svgs/LandingPlay';
-import ReviewsRibbon from '../../DesignTokens/Icon/svgs/ReviewsRibbon';
 
 const IconMap = {
+  cookbook: CookbookCollection,
+  latest: LatestReviews,
   play: LandingPlay,
-  shoppingCart: ShoppingCart,
+  recipeCard: RecipeCard,
   reviews: ReviewsRibbon,
+  rooster: CCORooster,
+  shoppingCart: ShoppingCart,
   trendingArrow: TrendingArrow,
+
 };
 
 export default IconMap;
