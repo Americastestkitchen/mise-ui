528d55d2990234d2f7c08879c927988e10ffb6f3
Jess Bon <jess.bon@americastestkitchen.com>
2022-02-08 08:45:00 -0800

    Adding CSSThemedProperties

---
+++ src/components/Ads/DisplayBeltAd/DisplayBeltAd.tsx
@@ -0,0 +1,173 @@
+import React from 'react';
+import styled, { css } from 'styled-components';
+import breakpoint from 'styled-components-breakpoint';
+import cloudinaryInstance, { baseImageConfig } from '../../../lib/cloudinary';
+import { color, font, fontSize, letterSpacing, withThemes } from '../../../styles';
+import { cssThemedColor } from '../../../styles/mixins';
+
+export type HeroImages = {
+  mobile: string,
+  tablet: string,
+  largeTablet: string,
+  desktop: string,
+}
+
+const Content = styled.div`
+  display: flex;
+  flex-direction: column;
+  margin-bottom: 3.2rem;
+  margin: 2.5rem 3.3rem 3.2rem auto;
+  text-align: right;
+
+  ${breakpoint('md')`
+    margin: 0 auto 1.6rem;
+    text-align: center;
+  `}
+`;
+
+const CtaTheme = {
+  default: css`
+    align-items: center;
+    color: ${color.white};
+    display: flex;
+    font: ${fontSize.lg}/2rem ${font.pnb};
+    height: 4.5rem;
+    justify-content: center;
+    letter-spacing: ${letterSpacing.cta};
+    margin: 0 auto;
+    width: 34rem;
+
+    span {
+    font-size: 3.1rem;
+    margin: 0.15rem 0 0 0.8rem;
+    }
+
+    ${breakpoint('md')`
+    width: 27.2rem;
+    `}
+  `,
+  atk: css`
+    background-color: ${color.coldPool};
+
+    &:hover {
+    background-color: ${color.darkColdPool};
+    }
+  `,
+  cio: css`
+    background-color: ${color.squirrel};
+
+    &:hover {
+    background-color: ${color.cork};
+    }
+  `,
+  cco: css`
+    background-color: ${color.denim};
+
+    &:hover {
+    background-color: ${color.arapawa};
+    }
+  `,
+};
+const Cta = styled.a`${withThemes(CtaTheme)}`;
+
+const Headline = styled.p`
+  font: ${fontSize.sm}/2.3rem ${font.mwr};
+  font-style: italic;
+  margin-bottom: 0.5rem;
+
+  ${breakpoint('md')`
+    font-size: 1.9rem;
+    line-height: 2rem;
+  `}
+  ${cssThemedColor}
+`;
+
+const SaleCopy = styled.h2`
+  font: 2.8rem/2.2rem ${font.pnb};
+
+  ${breakpoint('md')`
+    font-size: 3.2rem;
+    line-height: 3.3rem;
+  `}
+  ${cssThemedColor}
+`;
+
+const ImageBgWrapperTheme = {
+  default: css`
+    align-items: center;
+    display: flex;
+    flex-direction: column;
+    height: 19.2rem;
+    justify-content: center;
+    position: relative;
+    width: 100%;
+  
+    ${breakpoint('md')`
+      height: 15rem;
+    `}
+  `,
+  atk: css`
+    background-color: ${color.frost};
+  `,
+  cio: css`
+    background-color: ${color.ivory};
+  `,
+  cco: css`
+    background-color: ${color.aliceBlue};
+  `,
+};
+const ImageBgWrapper = styled.div`${withThemes(ImageBgWrapperTheme)}`;
+
+const Img = styled.img`
+  height: 100%;
+  left: 50%;
+  position: absolute;
+  top: 47%;
+  transform: translate(-50%, -50%);
+
+  ${breakpoint('md')`
+    top: 50%;
+  `}
+`;
+
+export type DisplayBeltAdProps = {
+  /** cloudinary strings for background images */
+  backgroundImages: HeroImages;
+  /** cta button copy */
+  ctaCopy?: string;
+  /** cta link */
+  ctaLink?: string;
+  /** top headline copy */
+  headline?: string;
+  /** sale copy  */
+  saleCopy?: string
+};
+
+const DisplayBeltAd = ({
+  backgroundImages,
+  ctaCopy = 'SAVE NOW',
+  ctaLink = 'https://shop.americastestkitchen.com',
+  headline = 'Discover favorite cookbooks',
+  saleCopy = 'Up to 70% off',
+}: DisplayBeltAdProps) => (
+  <ImageBgWrapper>
+    <picture>
+      <source media="(min-width: 1136px)" srcSet={cloudinaryInstance.url(backgroundImages.desktop, { ...baseImageConfig, height: 150 })} />
+      <source media="(min-width: 1024px)" srcSet={cloudinaryInstance.url(backgroundImages.largeTablet, { ...baseImageConfig, height: 150 })} />
+      <source media="(min-width: 768px)" srcSet={cloudinaryInstance.url(backgroundImages.tablet, { ...baseImageConfig, height: 150 })} />
+      <Img
+        src={cloudinaryInstance.url(backgroundImages.mobile, { ...baseImageConfig, height: 130 })}
+        alt=""
+        crossOrigin="anonymous"
+        decoding="async"
+      />
+    </picture>
+    <Content>
+      <Headline>{headline}</Headline>
+      <SaleCopy>{saleCopy}</SaleCopy>
+    </Content>
+    <Cta href={ctaLink}>{ctaCopy}<span>‣</span></Cta>
+  </ImageBgWrapper>
+);
+
+export default DisplayBeltAd;
