a1063931d5711bde32927ea668562f0a80fd43e6
Harry Scheuerle <harry.scheuerle@americastestkitchen.com>
2022-02-09 11:07:38 -0500

    DisplayBeltAd root index exports

--- src/index.js
+++ src/index.js
@@ -89,6 +89,9 @@ import fonts from './styles/fonts';
 import globalStyle from './styles/global';
 import { color, font, fontSize, mixins, spacing } from './styles';
 
+export { default as DisplayBeltAd } from './components/Ads/DisplayBeltAd';
+export * from './components/Ads/DisplayBeltAd';
+
 export { default as RelatedContentCard } from './components/Cards/RelatedContentCard';
 export * from './components/Cards/RelatedContentCard';
 
