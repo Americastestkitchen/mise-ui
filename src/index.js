c7f74bec682c32f96368fad39a380700231e60ef
Harry Scheuerle <harry.scheuerle@americastestkitchen.com>
2022-04-04 09:43:28 -0400

    remove unused

--- src/index.js
+++ src/index.js
@@ -122,7 +122,6 @@ export { default as BaseCarousel } from './components/Carousels/BaseCarousel';
 export * from './components/Cards/RelatedRecipeCard';
 export { default as RelatedRecipeCard } from './components/Cards/RelatedRecipeCard';
 
-
 export * from './components/Cards/ArticleCard';
 export { default as ArticleCard } from './components/Cards/ArticleCard';
 
