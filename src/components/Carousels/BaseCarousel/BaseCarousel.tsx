ff759c7ebf38d1ac4a1eea09f89b43e9f6186fde
Harry Scheuerle <harry.scheuerle@americastestkitchen.com>
2022-05-31 12:09:18 -0400

    favorites: add suggestion logic as side effect

--- src/components/Carousels/BaseCarousel/BaseCarousel.tsx
+++ src/components/Carousels/BaseCarousel/BaseCarousel.tsx
@@ -1,13 +1,13 @@
 /* eslint-disable @typescript-eslint/no-explicit-any */
-import type Flickity from 'flickity';
-import React, { PropsWithChildren, useCallback, createContext, useContext, useEffect } from 'react';
+import Flickity from 'flickity';
+import React, { PropsWithChildren, useCallback, createContext, useContext } from 'react';
 import ArrowButton from './ArrowButton';
 
 import { Title, Header, Navigation, Divider, Carousel } from './styled-elements';
 import useFlickity, { FlickityState } from './useFlickity';
 
 export const context = createContext({
-  registerOnChange: (callback: (flickity: Flickity) => void): (() => void) => {
+  registerOnChange: (callback: (flickity: Flickity, ev?: number) => void): (() => void) => {
     // eslint-disable-next-line no-console
     console.log('useCarouselContext hook outside of provider', callback);
     return () => {
@@ -23,6 +23,7 @@ export const context = createContext({
     // eslint-disable-next-line no-console
     console.log('useCarouselContext hook outside of provider');
   },
+  flickity: null as (Flickity | null),
 });
 
 export const useCarouselContext = () => useContext(context);
@@ -57,7 +58,7 @@ export default function BaseCarousel({
 
   const registerOnChange = useCallback((callback) => {
     callback(flickity.current);
-    flickity.current?.on('change', () => callback(flickity.current));
+    flickity.current?.on('change', ev => callback(flickity.current, ev));
     return () => {
       flickity.current?.off('change', callback);
     };
@@ -66,60 +67,6 @@ export default function BaseCarousel({
   const resize = useCallback(() => {
     flickity.current?.resize();
   }, [flickity]);
-
-  const publishEvent = useCallback((evt, data) => {
-    if (typeof window.dry !== 'undefined') {
-      // eslint-disable-next-line no-undef
-      window.dry.events.publish(evt, data);
-    }
-  }, []);
-
-  const handleCellChange = useCallback((idx) => {
-    const cell = (flickity.current?.cells[idx] as any).element;
-    const node = cell.cloneNode(true);
-    publishEvent('flickity:change', { node, idx });
-  }, [flickity, publishEvent]);
-
-  const handleCellClick = useCallback((evt: React.FocusEvent<HTMLElement>) => {
-    const button = evt.target.closest('button');
-    if (button) {
-      const list = button.classList;
-      if (list.contains('remove-cell')) {
-        evt.preventDefault();
-        if ((flickity.current?.slides || []).length > 1) {
-          const { selectedIndex } = flickity.current || {};
-          publishEvent('flickity:remove', { ...button.dataset, selectedIndex });
-
-          setTimeout(() => {
-            const cell = button.closest('.suggestion-card');
-            if (cell) flickity.current?.remove(cell);
-          }, 500);
-
-          if ((flickity.current?.cells || []).length < 11) {
-            setTimeout(() => {
-              flickity.current?.next(true);
-            }, 2500);
-          }
-        } else {
-          publishEvent('flickity:remove', { ...button.dataset });
-          flickity.current?.destroy();
-        }
-      } else if (list.contains('next-cell')) {
-        flickity.current?.next();
-        publishEvent('flickity:next', null);
-      } else if (list.contains('previous-cell')) {
-        flickity.current?.previous(true);
-        publishEvent('flickity:previous', null);
-      }
-    }
-  }, [flickity, publishEvent]);
-
-  useEffect(() => {
-    const container = (flickity.current as any)?.element;
-    container.addEventListener('click', handleCellClick);
-    flickity.current?.on('change', handleCellChange);
-    return () => container.removeEventListener('click', handleCellClick);
-  }, [flickity, handleCellClick, handleCellChange]);
 
   const carouselLabel = `Carousel: ${title}`;
 
@@ -134,8 +81,8 @@ export default function BaseCarousel({
       </Header>
       <Divider showDivider={showDivider} />
       {/* IMPORTANT: Cards use this internally `.carousel & {}` to avoid breakpoint styles */}
-      <Carousel data-qa="carousel-wrapper" ref={flickityRef} className="carousel">
-        <context.Provider value={{ registerOnChange, onFocus, resize }}>
+      <Carousel ref={flickityRef} className="carousel">
+        <context.Provider value={{ registerOnChange, onFocus, resize, flickity: flickity.current }}>
           {children}
         </context.Provider>
       </Carousel>
