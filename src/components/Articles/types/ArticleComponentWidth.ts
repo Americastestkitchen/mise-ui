04efbe8befdfe10ce0c10a522f749fe04bbb9434
Tom Tornese <tornese.tom@gmail.com>
2022-06-09 16:19:16 -0400

    FEA-25: converting PullQuote component and stories to typescript

---
+++ src/components/Articles/types/ArticleComponentWidth.ts
@@ -0,0 +1,7 @@
+/*
+ * This type maps to editorial options for article components in barista.
+ * Editors can set any article component's width to 'default' or 'wide'.
+ */
+type ArticleComponentWidthType = 'default' | 'wide';
+
+export default ArticleComponentWidthType;
