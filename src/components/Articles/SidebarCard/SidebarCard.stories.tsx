a7b12f8271040540b3569a687c9e3e74ceef0dee
Tom Tornese <tornese.tom@gmail.com>
2022-06-16 10:38:54 -0400

    FEA-27: converting SidebarCard and ArticleFigcaption components to typescript

---
+++ src/components/Articles/SidebarCard/SidebarCard.stories.tsx
@@ -0,0 +1,47 @@
+import React from 'react';
+import type { ComponentMeta, ComponentStory } from '@storybook/react';
+
+import SidebarCard from './index';
+import { siteKey } from '../../../config/argTypes';
+import { addThemedWrapper } from '../../../config/decorators';
+
+export default {
+  title: 'Components/Articles/SidebarCard',
+  component: SidebarCard,
+  decorators: [addThemedWrapper()],
+  argTypes: { siteKey },
+} as ComponentMeta<typeof SidebarCard>;
+
+const Template: ComponentStory<typeof SidebarCard> = props => <SidebarCard {...props} />;
+
+const description = 'We’ve happily made do with Weber’s basic kettle for years. But would newer, more tricked-out charcoal cookers be worth the upgrade?';
+const photo = 'TnT/2020/1_CCJJ_Dill%20Pickles/SPS_Pickle_Samples_with_Brine_104-1';
+const sharedArgs = {
+  altText: 'picture of a thing',
+  siteKey: 'atk',
+  title: 'FAQ About Storing Blue Cheese',
+  type: 'Article',
+  url: 'https://www.americastestkitchen.com/articles/3236-this-hardware-store-staple-deodorizes-your-fridge-better-than-baking-soda',
+};
+
+export const ImageAndDescription = Template.bind({});
+ImageAndDescription.args = {
+  ...sharedArgs,
+  description,
+  photo,
+};
+
+export const ImageNoDescription = Template.bind({});
+ImageNoDescription.args = {
+  ...sharedArgs,
+  photo,
+};
+
+export const DescriptionNoImage = Template.bind({});
+DescriptionNoImage.args = {
+  ...sharedArgs,
+  description,
+};
+
+export const NoDescriptionNoImage = Template.bind({});
+NoDescriptionNoImage.args = { ...sharedArgs };
