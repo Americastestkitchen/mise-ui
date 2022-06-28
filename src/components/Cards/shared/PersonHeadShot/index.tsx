a124f6b9bf69e69506f2fcff9162725bc36659c1
Audrey Magwili <audrey.magwili@americastestkitchen.com>
2022-06-28 16:38:53 -0700

    optimize css

--- src/components/Cards/shared/PersonHeadShot/index.tsx
+++ src/components/Cards/shared/PersonHeadShot/index.tsx
@@ -1,6 +1,6 @@
 import React from 'react';
 import styled, { css } from 'styled-components';
-import { md, lg } from '../../../../styles/breakpoints';
+import { sm, md, lg } from '../../../../styles/breakpoints';
 import { getImageUrl } from '../../../../lib/cloudinary';
 
 type HeadshotSize = {
@@ -16,30 +16,24 @@ type PersonHeadshotPropTypes = {
 }
 
 const PersonHeadShotWrapper = styled.div<{ size: HeadshotSize }>`
-  ${({ size }) => (
-    css`
-      border-radius: 50%;
-      height: ${size.sm}rem;
-      width: ${size.sm}rem;
-    `
-  )}
+  flex-shrink: 0;
+  overflow: hidden;
+  border-radius: 50%;
 
   ${({ size }) => `
+    height: ${size.sm}rem;
+    width: ${size.sm}rem;
+
     ${md(css`
       height: ${size.md || size.sm}rem;
       width: ${size.md || size.sm}rem;
     `)}
-  `}
 
-  ${({ size }) => `
     ${lg(css`
       height: ${size.lg || size.md || size.sm}rem;
       width: ${size.lg || size.md || size.sm}rem;
     `)}
   `}
-
-  flex-shrink: 0;
-  overflow: hidden;
 `;
 
 const PersonHeadShotImg = styled.img`
