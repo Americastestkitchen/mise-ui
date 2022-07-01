77c0a986ee88c61fafb12d75fbae98c14748373d
Audrey Magwili <audrey.magwili@americastestkitchen.com>
2022-07-01 10:26:37 -0700

    make sticker tsx

---
+++ src/components/Cards/shared/Sticker/Sticker.tsx
@@ -0,0 +1,116 @@
+import React from 'react';
+import styled from 'styled-components';
+import { color, font, fontSize, letterSpacing, spacing } from '../../../../styles';
+import { Collection, VideoPlay } from '../../../DesignTokens/Icon';
+
+const stickerHeight = '1.8rem';
+
+type StickerContentType = 'collection' | 'clip' | 'episode' | 'playlist' | 'video' | 'cooking school course';
+
+export type StickerPropType = {
+  className?: string;
+  contentType?: StickerContentType,
+  icon?: boolean,
+  type: string,
+  text: string
+};
+
+const StyledSticker = styled.span<{ type: string }>`
+  display: inline-flex;
+  align-items: center;
+  margin-bottom: ${spacing.xsm};
+  margin-left: ${spacing.xsm};
+  padding-left: ${spacing.xsm};
+  padding-right: ${spacing.xsm};
+  height: ${stickerHeight};
+  border-radius: 0.6rem;
+  background-color: ${({ type }) => (type === 'priority' ? color.tomato : color.transparentBlack)};
+  color: ${color.white};
+  font: ${fontSize.xsm}/${stickerHeight} ${font.pnb};
+  letter-spacing: ${letterSpacing.md};
+  text-transform: uppercase;
+  vertical-align: top;
+  white-space: nowrap;
+
+  svg {
+    margin-right: ${spacing.xxsm};
+    width: 1rem;
+    height: 1rem;
+    max-height: 60%;
+  }
+
+  @media print {
+    background-color: ${color.black};
+  }
+`;
+
+const determineIconType = (contentType: StickerContentType) => {
+  const contentTypes = {
+    collection: Collection,
+    clip: VideoPlay,
+    episode: VideoPlay,
+    playlist: VideoPlay,
+    video: VideoPlay,
+    'cooking school course': VideoPlay,
+  };
+  const El = contentTypes[contentType];
+  return El ? <El fill={`${color.white}`} /> : null;
+};
+
+/**
+
+  A sticker surfaces ‘make or break’ information in order to help the user
+  make a smarter decision between cards in the grid. Examples: “vegetarian” or
+  “gluten free” or “popular” “editor’s pick”
+
+<br>
+
+-----
+
+<br>
+
+### Notes
+
+Stickers are not interactive, only informative, so there is no hover state.
+
+There are two types of stickers, priority and basic. Basic stickers have
+a `transparentBlack` background. Priority stickers are reserved for
+messages like “new” or “price drop”, and have a red (`tomato`) background.
+
+It can have or not have an icon (collections icon, video icon).
+
+A desktop card can have two stickers, max; one sticker max on mobile
+
+Text does not wrap
+
+Line height of the text and the height of pill should both be set to `1.5rem`
+to vertically center text.
+
+Stickers scale down to a smaller size (`height: 1.2rem;` `font-size: 0.8rem;`)
+for mobile for most types of cards.
+
+<br>
+
+-----
+
+<br>
+
+*/
+
+export default function Sticker({
+  className = '',
+  contentType,
+  icon,
+  text,
+  type,
+}: StickerPropType) {
+  return (
+    <StyledSticker
+      className={className}
+      type={type}
+    >
+      { icon && contentType && type === 'editorial' ? determineIconType(contentType) : null }
+      <span dangerouslySetInnerHTML={{ __html: text }} />
+    </StyledSticker>
+  );
+}
