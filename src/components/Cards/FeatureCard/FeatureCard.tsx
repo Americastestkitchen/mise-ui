07d80dfb57533573b1c3874f61560509241fa85c
Audrey Magwili <audrey.magwili@americastestkitchen.com>
2022-07-01 11:31:48 -0700

    update references

--- src/components/Cards/FeatureCard/FeatureCard.tsx
+++ src/components/Cards/FeatureCard/FeatureCard.tsx
@@ -7,7 +7,7 @@ import { md, untilLg } from '../../../styles/breakpoints';
 import Badge from '../../Badge/Badge';
 import FavoriteRibbonWithBg from '../shared/FavoriteRibbonWithBg/FavoriteRibbonWithBg';
 import Image from '../shared/Image';
-import PersonHeadShot, { PersonHeadshotPropTypes } from '../shared/PersonHeadShot';
+import PersonHeadShot, { PersonHeadshotPropTypes } from '../shared/PersonHeadShot/PersonHeadShot';
 import Sticker from '../shared/Sticker';
 import Title from '../shared/Title';
 import { BaseCardPropType } from '../Cards';
