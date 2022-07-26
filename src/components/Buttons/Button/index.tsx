ad5506f6feca84724825331bd0c3adf30c741216
Harry Scheuerle <harry.scheuerle@americastestkitchen.com>
2022-07-26 15:08:00 -0400

    WEB-31: external focus-visible overridding css

--- src/components/Buttons/Button/index.tsx
+++ src/components/Buttons/Button/index.tsx
@@ -28,7 +28,9 @@ const StyledButtonTheme = {
     transition: 0.2s all ease, 0s outline-offset linear, 0s outline linear;
     white-space: nowrap;
     &:focus {
-      ${mixins.focusIndicator()}
+      /* Not using mixin and using important for firefox agent css issues */
+      outline: 2px dotted ${color.eclipse} !important;
+      outline-offset: 3px;
       z-index: 1;
     }
 
