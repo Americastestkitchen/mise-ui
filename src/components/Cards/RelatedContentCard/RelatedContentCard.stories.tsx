80db47a487f01fdba4a257b03896ceb326dbbab4
Harry Scheuerle <harry.scheuerle@americastestkitchen.com>
2022-08-02 10:49:10 -0400

    WEB-185: Add styled markup to RelatedContentCard

--- src/components/Cards/RelatedContentCard/RelatedContentCard.stories.tsx
+++ src/components/Cards/RelatedContentCard/RelatedContentCard.stories.tsx
@@ -20,7 +20,7 @@ const defaultArgs = {
   src: 'https://res.cloudinary.com/hksqkdlah/image/upload/c_fill,f_auto,g_faces:auto,q_auto:low,w_200,ar_1:1/v1592840035/mise-play/feature-card-wide.jpg',
   headline: 'Recipe',
   title: 'Whole Roast Snapper with Citrus Vinaigrette Vinaigrette',
-  body: 'To serve up an impressive dish of roasted red snapper, we started by making shallow slashes in the skin to ensure even cooking and seasoning; this step also allowed us to gauge the doneness of the fish easily.',
+  body: '<em>To serve up</em> an <strong>impressive</strong> dish of <a>roasted red snapper, we started by making shallow slashes</a> in the skin to ensure even cooking and seasoning; this step also allowed us to gauge the doneness of the fish easily.',
   siteKey: 'atk',
 };
 
