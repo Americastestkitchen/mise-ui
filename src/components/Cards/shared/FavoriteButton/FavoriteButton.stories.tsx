85610bd760dbef8c427a877a83281dc47564919f
Audrey Magwili <audrey.magwili@americastestkitchen.com>
2022-08-03 13:52:27 -0700

    stories file

--- src/components/Cards/shared/FavoriteButton/FavoriteButton.stories.tsx
+++ src/components/Cards/shared/FavoriteButton/FavoriteButton.stories.tsx
@@ -1,19 +1,32 @@
 import React from 'react';
-import { action } from '@storybook/addon-actions';
-
+import { ComponentMeta, ComponentStory } from '@storybook/react';
 import FavoriteButton from './FavoriteButton';
 
 export default {
   title: 'Components/Cards/shared/FavoriteButton',
   component: FavoriteButton,
-};
+  argTypes: {
+    siteKey: {
+      control: {
+        type: 'select',
+        options: [
+          'atk', 'cco', 'cio',
+        ],
+      },
+    },
+    isFavorited: {
+      control: {
+        type: 'boolean',
+      },
+    },
+  },
+} as ComponentMeta<typeof FavoriteButton>;
 
 export const Default = () => (
   <FavoriteButton
     className="favorite-ribbon"
     isFavorited={false}
     objectId="123"
-    onClick={action('click-button')}
     siteKey="atk"
     title=""
   />
@@ -24,8 +37,19 @@ export const Favorited = () => (
     className="favorite-ribbon"
     isFavorited
     objectId="123"
-    onClick={action('click-button')}
     siteKey="atk"
     title=""
   />
 );
+
+const Template: ComponentStory<typeof FavoriteButton> = args => <FavoriteButton {...args} />;
+
+export const Primary = Template.bind({});
+
+Primary.args = {
+  className: 'favorite-ribbon',
+  isFavorited: false,
+  objectId: '123',
+  siteKey: 'atk',
+  title: '',
+};
