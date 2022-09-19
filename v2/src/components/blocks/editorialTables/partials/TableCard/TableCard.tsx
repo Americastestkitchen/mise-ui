9e9036a13ab4e677862c4145fd8fb82578bd282c
Tom Tornese <tornese.tom@gmail.com>
2022-09-19 14:41:02 -0400

    feat: :sparkles: refactor InformationalTable into EditorialTable, add support for Comparison tables

--- v2/src/components/blocks/editorialTables/partials/TableCard/TableCard.tsx
+++ v2/src/components/blocks/editorialTables/partials/TableCard/TableCard.tsx
@@ -1,63 +1,61 @@
 import React from 'react';
 
-import CircularIcon from '../../../../partials/CircularIcon/CircularIcon';
+import TableCell from '../TableCell/TableCell';
 import styles from './tableCard.module.scss';
-import { EditorialTable } from '../../types';
+import { EditorialTableType, EditorialTableCell } from '../../types';
 
 type TableCardProps = {
   currentPage: number;
-  table: EditorialTable;
+  table: EditorialTableType;
 };
 
-const TableCard = ({ currentPage, table }: TableCardProps) => (
-  <table className={styles.table}>
-    <tbody>
-      {
-        table.map(row => (
-          <tr className={styles.row} key={row.id}>
-            {row.cells.slice(currentPage).map((cell, index: number) => {
-              const { content, id, type } = cell;
-              const cellClassNames = `${index >= 3 && styles.oob} ${styles.cell} ${styles[type]}`;
-              switch(type) {
-                case 'header':
-                  return (
-                    <th className={cellClassNames} key={id}>
-                      {content}
-                    </th>
-                  );
-                case 'icon': 
-                  // TODO: may need to revisit logic for rendering yes or no icons
-                  // right now api is set up to return the strings 'yes' or 'no'
-                  // when icon cell type is selected
-                  return (
-                    <td className={cellClassNames} key={id}>
-                      <hr aria-hidden="true" className={styles.divider} />
-                      <div className={styles.iconWrapper}>
-                        <div className={`${styles[content]} ${styles.iconSvg}`}>
-                          <CircularIcon type={content === 'yes' ? 'checkmark' : 'close'} />
-                        </div>
-                        {content}
-                      </div>
-                    </td>
-                  );
-                case 'text':
-                  return (
-                    <td className={cellClassNames} key={id}>
-                      <hr aria-hidden="true" className={styles.divider}/>
-                      <span dangerouslySetInnerHTML={{ __html: content }} />
-                    </td>
-                  );
-                default:
-                  return (
-                    <td key={id} className={cellClassNames}></td>
-                  );
-              }
-            })}
-          </tr>
-        ))
-      }
-    </tbody>
-  </table>
-);
+const TableCard = ({ currentPage, table }: TableCardProps) => {
+  const { rows, type } = table;
+  return (
+    <table className={styles.table}>
+      <tbody>
+        {
+          rows.map((row, rowIndex) => {
+            let pageSlice = currentPage;
+            let rowHeader: EditorialTableCell | undefined = undefined;
+            if (type === 'comparison') {
+              rowHeader = row.cells[0];
+              pageSlice = currentPage + 1;
+            }
+            return (
+              <tr className={styles.row} key={row.id}>
+                {
+                  rowHeader ? (
+                    <TableCell
+                      {...rowHeader}
+                      isInFirstRow={rowIndex === 0}
+                      indexInRow={0}
+                      tableType={type}
+                    />
+                  ) : null
+                }
+                <>
+                  {
+                    row.cells.slice(pageSlice).map((cell, index) => {
+                      return (
+                        <TableCell
+                          {...cell}
+                          key={cell.id}
+                          rowHeaderContent={rowHeader?.content}
+                          indexInRow={index}
+                      tableType={type}
+                        />
+                      )
+                  })}
+                </>
+              </tr>
+            );
+          })
+        }
+      </tbody>
+    </table>
+
+  );
+};
 
 export default TableCard;
