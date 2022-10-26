95535b69d913960699c410e394021d4293dd1c29
bradwaropay <brad.waropay@gmail.com>
2022-10-26 08:04:21 -0700

    Add z-indexes

--- v2/src/components/tokens/layout/Layout.ts
+++ v2/src/components/tokens/layout/Layout.ts
@@ -2,6 +2,7 @@ import layout from "../../../styles/_tokens/_layout.scss";
 import { getTokenObject } from '../utils' 
 
 export const Breakpoints = getTokenObject('breakpoint', layout);
+export const ZIndex = getTokenObject('zIndex', layout);
 export const SpacingUnit = layout.spacingUnit;
 export const Spacing = getTokenObject('spacing', layout);
 export const Content = getTokenObject('content', layout);
