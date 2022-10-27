9c1c803f56636c23be06201534d25a8bad66c948
Audrey Magwili <audrey.magwili@americastestkitchen.com>
2022-10-27 15:09:44 -0700

    stories file

---
+++ v2/src/components/blocks/HeroCard/HeroCard.stories.tsx
@@ -0,0 +1,18 @@
+import React from "react";
+
+import { ComponentStory, ComponentMeta } from '@storybook/react';
+import HeroCard from "./HeroCard";
+
+export default {
+  title: "Components/Blocks/HeroCard",
+  component: HeroCard,
+} as ComponentMeta<typeof HeroCard>;
+
+export const Template: ComponentStory<typeof HeroCard> = args => {
+  return <HeroCard {...args} />;
+};
+
+
+// const Template: ComponentStory<typeof Accordion> = args => <Accordion {...args} />;
+
+export const HeroCardStory: ComponentStory<typeof HeroCard> = Template.bind({});
