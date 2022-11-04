dd4f86822ec8baae380cac619ea170c96f4fccd6
Audrey Magwili <audrey.magwili@americastestkitchen.com>
2022-11-03 17:00:05 -0700

    image setup

--- v2/src/components/blocks/HeroCard/HeroCard.tsx
+++ v2/src/components/blocks/HeroCard/HeroCard.tsx
@@ -3,12 +3,21 @@ import React from "react";
 import styles from "./styles/HeroCard.module.scss";
 
 export interface HeroCardProps {
+  image?: { altText?: string, cloudinaryUrl: string };
 }
 
-export const HeroCard: React.FC<HeroCardProps> = ({ }: HeroCardProps) => {
+export const HeroCard: React.FC<HeroCardProps> = ({ image }: HeroCardProps) => {
   return (
     <div className={styles.container}>
-      <div>
+      <div className={styles.info}>
+        <img
+          className={styles.headShot}
+          crossOrigin="anonymous"
+          decoding="async"
+          alt={image?.altText}
+          src={image?.cloudinaryUrl}
+          // onError={() => { setImageError(true); }}
+        />
         <img src="/" alt="Tomato Sandwich" />
         <h1>Your Summer Needs These Three Tomato Sandwiches</h1>
       </div>
