ad3cff430f579e35b721d85ab38bb42a6d13de18
Audrey Magwili <audrey.magwili@americastestkitchen.com>
2022-11-08 14:11:08 -0800

    gradient fixed

--- v2/src/components/blocks/HeroCard/HeroCard.tsx
+++ v2/src/components/blocks/HeroCard/HeroCard.tsx
@@ -16,23 +16,21 @@ export const HeroCard: React.FC<HeroCardProps> = ({
 }: HeroCardProps) => {
   return (
     <div className={styles.container}>
-      <div className={styles.info}>
-        {image && (
-          <img
-            className={styles.heroImage}
-            alt={image?.altText}
-            src={image?.cloudinaryUrl}
-          />
-        )}
-        <div className={styles.text}>
-          <h1 className={styles.headline}>{headlineText}</h1>
-          <div className={styles.attribution}>
-            <BylineList authors={[author]} />
-            <svg width="21" height="30" viewBox="0 0 21 30" fill="none" xmlns="http://www.w3.org/2000/svg">
-            <path d="M10.483 18.809L3 24V3H18V24L10.483 18.809Z" stroke="white" stroke-width="2" stroke-miterlimit="10"/>
-            </svg>
-            {/* TODO: get actual bookmark imported */}
-          </div>
+      {image && (
+        <img
+          className={styles.heroImage}
+          alt={image?.altText}
+          src={image?.cloudinaryUrl}
+        />
+      )}
+      <div className={styles.text}>
+        <h1 className={styles.headline}>{headlineText}</h1>
+        <div className={styles.attribution}>
+          <BylineList authors={[author]} />
+          <svg width="21" height="30" viewBox="0 0 21 30" fill="none" xmlns="http://www.w3.org/2000/svg">
+          <path d="M10.483 18.809L3 24V3H18V24L10.483 18.809Z" stroke="white" stroke-width="2" stroke-miterlimit="10"/>
+          </svg>
+          {/* TODO: get actual bookmark imported */}
         </div>
       </div>
     </div>
