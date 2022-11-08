2afc97f447c87d9a47eef87c4c9469d017b225e5
Audrey Magwili <audrey.magwili@americastestkitchen.com>
2022-11-08 08:38:23 -0800

    headline prop

--- v2/src/components/blocks/HeroCard/HeroCard.tsx
+++ v2/src/components/blocks/HeroCard/HeroCard.tsx
@@ -6,11 +6,13 @@ import styles from "./styles/HeroCard.module.scss";
 export type HeroCardProps = {
   image?: { altText?: string, cloudinaryUrl: string };
   author: Author;
+  headlineText: string;
 }
 
 export const HeroCard: React.FC<HeroCardProps> = ({
   image,
   author,
+  headlineText,
 }: HeroCardProps) => {
   return (
     <div className={styles.container}>
@@ -22,6 +24,16 @@ export const HeroCard: React.FC<HeroCardProps> = ({
             src={image?.cloudinaryUrl}
           />
         )}
+        <div className={styles.text}>
+          <h1 className={styles.headline}>{headlineText}</h1>
+          <div className={styles.attribution}>
+            <BylineList authors={[author]} />
+            <svg width="21" height="30" viewBox="0 0 21 30" fill="none" xmlns="http://www.w3.org/2000/svg">
+            <path d="M10.483 18.809L3 24V3H18V24L10.483 18.809Z" stroke="white" stroke-width="2" stroke-miterlimit="10"/>
+            </svg>
+            {/* TODO: get actual bookmark imported */}
+          </div>
+        </div>
       </div>
     </div>
   );
