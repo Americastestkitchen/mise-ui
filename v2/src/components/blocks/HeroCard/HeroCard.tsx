248b323ea0069f6636b427ddf433a949f40e4e90
bradwaropay <brad.waropay@gmail.com>
2022-11-13 10:06:31 -0700

    Update HeroCard

--- v2/src/components/blocks/HeroCard/HeroCard.tsx
+++ v2/src/components/blocks/HeroCard/HeroCard.tsx
@@ -1,39 +1,51 @@
 import React from "react";
-import BylineList, { Author } from "../../partials/Byline/Byline";
+import Byline, { Author } from "../../partials/Byline/Byline";
 
 import styles from "./styles/HeroCard.module.scss";
 
 export type HeroCardProps = {
-  image?: { altText?: string, cloudinaryUrl: string };
-  author: Author;
-  headlineText: string;
+  image: { 
+    altText: string,
+    url: string
+  };
+  author: Author,
+  headline: string,
+  favorite?: boolean,
 }
 
 export const HeroCard: React.FC<HeroCardProps> = ({
   image,
   author,
-  headlineText,
-}: HeroCardProps) => {
+  headline,
+  favorite = false,
+}) => {
   return (
-    <div className={styles.container}>
-      {image && (
+    <article className={styles.container}>
+      {/* TODO: Add Link component */}
+      <a
+        className={styles["image-link"]}
+        href="#"
+      >
         <img
-          className={styles.heroImage}
+          className={styles["image"]}
           alt={image.altText}
-          src={image.cloudinaryUrl}
+          src={image.url}
         />
-      )}
-      <div className={styles.text}>
-        <h1 className={styles.headline}>{headlineText}</h1>
-        <div className={styles.attribution}>
-          <BylineList authors={[author]} />
-          <svg width="21" height="30" viewBox="0 0 21 30" fill="none" xmlns="http://www.w3.org/2000/svg">
-          <path d="M10.483 18.809L3 24V3H18V24L10.483 18.809Z" stroke="white" stroke-width="2" stroke-miterlimit="10"/>
-          </svg>
-          {/* TODO: get actual bookmark imported */}
-        </div>
+      </a>
+      <div className={styles.details}>
+        <h1 className={styles.headline}>
+          {/* TODO: Add Link component */}
+          <a href="#">{headline}</a>
+        </h1>
+        <footer className={styles.footer}>
+          <Byline
+            authors={[author]}
+            theme="light"
+          />
+          {/* TODO: Add Favorite Toggle */}
+        </footer>
       </div>
-    </div>
+    </article>
   );
 };
 
