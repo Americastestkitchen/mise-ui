f0cbcd7612649ed86c5848999578449f2d43f020
bradwaropay <brad.waropay@gmail.com>
2022-11-13 22:56:58 -0700

    Add feed styling

---
+++ v2/src/components/blocks/ContentFeed/ContentFeed.tsx
@@ -0,0 +1,60 @@
+
+
+
+import StandardPeekCard from '../PeekCard/StandardPeekCard/StandardPeekCard';
+import { StandardCard, VideoCard } from '../PeekCard/PeekCard';
+import StandardButton from '../../partials/Buttons/StandardButton/StandardButton';
+
+import styles from "./ContentFeed.module.scss";
+
+type FeedContent = StandardCard | VideoCard
+
+export interface ContentFeedProps {
+  className?: string;
+  headline?: string;
+  feed: FeedContent[];
+  onClick?(): void;
+}
+
+const ContentFeed: React.FC<ContentFeedProps> = ({
+  className,
+  headline,
+  onClick,
+  feed
+}) => {
+  return (
+    <section className={`${styles["section"]} ${className}`}>
+      { !!headline &&
+        <header className={styles["header"]}>
+          <h2 className={styles["header__headline"]}>{headline}</h2>
+        </header>
+      }
+      <ul className={styles["feed-list"]}>
+        {feed.map((content, i) => {
+          return ( 
+            <li
+              key={i}
+              className={styles["feed-list__item"]}
+            >
+              {content.cardType === 'standard' && (
+                <StandardPeekCard card={content} />
+              )}
+            </li>
+          )
+        })
+      }
+      </ul>
+      { !!onClick &&
+        <footer className={styles["footer"]}>
+          <StandardButton
+            className={styles["footer__load-more"]}
+            label="Load More"
+            onClick={onClick}
+          />
+        </footer>
+      }
+    </section>
+  )
+}
+
+export default ContentFeed
