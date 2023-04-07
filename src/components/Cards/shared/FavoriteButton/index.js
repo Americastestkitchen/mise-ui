5a5e3e89e140fd5345b4c30cd4ad327d45fae945
Jess Bon <jess.bon@americastestkitchen.com>
2023-04-07 07:44:15 -0700

    Removing comments

--- src/components/Cards/shared/FavoriteButton/index.js
+++ src/components/Cards/shared/FavoriteButton/index.js
@@ -117,7 +117,7 @@ const FavoriteButton = ({
     data-origin-site={siteKey}
     data-testid="favorite-button"
     fill={fill}
-    onClick={meteredOnClick} // NOTE: for the Save button
+    onClick={meteredOnClick}
   >
     <FavoriteRibbon
       ariaHidden
