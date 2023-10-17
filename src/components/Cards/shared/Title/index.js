3df9590e147880836c42df4e385d02bab3939809
Audrey Magwili <audrey.magwili@americastestkitchen.com>
2023-10-16 22:48:22 -0700

    one liner

--- src/components/Cards/shared/Title/index.js
+++ src/components/Cards/shared/Title/index.js
@@ -5,8 +5,7 @@ import { color, fontSize, lineHeight, withThemes } from '../../../../styles';
 
 const TitleTheme = {
   default: css`
-    font: ${fontSize.xl}/${lineHeight.sm} proxima-nova, sans-serif;
-    font-weight: 700;
+    font: ${fontSize.xl}/${lineHeight.sm} proxima-nova, sans-serif 700;
     transition: color 0.2s ease;
 
     @media(hover: hover) {
